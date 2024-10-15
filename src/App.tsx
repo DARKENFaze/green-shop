16618be529007c1f6f1ab527f0b0930d7e18af74
DARKENFaze <otashblogs@gmail.com>
2024-10-15 13:26:03 +0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-// Sahifalar komponentlarini import qilish
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import PlantCare from "./pages/PlantCare";
 import Blogs from "./pages/Blogs";
 import "./App";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,7 @@ const App: React.FC = () => {
       <div className="app-wrapper">
         <Header />
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/plant-care" element={<PlantCare />} />
           <Route path="/blogs" element={<Blogs />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../sass/Home.scss";
-
-// Rasmlarni to'g'ridan-to'g'ri import qilish
 import bigFlower from "../assets/img/bigFlower.svg";
 import smallFlower from "../assets/img/smallFlower.svg";
+import btnRightTo from "../assets/icons/Arrow - Right.svg";
 
-const Banner: React.FC = () => {
+const Home: React.FC = () => {
   const [bannerData, setBannerData] = useState<any>(null);
 
   useEffect(() => {
@@ -20,21 +19,24 @@ const Banner: React.FC = () => {
     <div className="container">
       <section className="main-banner">
         <div className="banner-content">
-          <p>{bannerData.welcomeMessage}</p>
+          <p className="welCome">{bannerData.welcomeMessage}</p>
           <h1>
             {bannerData.title} <span>{bannerData.highlightedWord}</span>
           </h1>
           <p className="lorem-text">{bannerData.description}</p>
-          <button className="banner-btn">{bannerData.buttonText}</button>
+          <button className="banner-btn">
+            {bannerData.buttonText}
+            <img src={btnRightTo} alt="" />
+          </button>
         </div>
 
         <div className="flowers">
           <img className="smallFlower" src={smallFlower} alt="Small Flower" />
-          <img src={bigFlower} alt="Big Flower" />
+          <img className="bigFlower" src={bigFlower} alt="Big Flower" />
         </div>
       </section>
     </div>
   );
 };
 
-export default Banner;
+export default Home;
